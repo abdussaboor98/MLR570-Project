9787644d86855a75b9b09afa554d14fe135a16cd
abdussaboor98 <gaffari98@gmail.com>
2024-11-30 20:54:05 +0400

    changed n_points

--- hdbscan lgbm cls all.py
+++ hdbscan lgbm cls all.py
@@ -241,6 +241,7 @@ bayes_cv = BayesSearchCV(
     n_iter=50,
     cv=5,
     n_jobs=-1,
+    n_points=5,
     scoring='f1',
     random_state=42
 )
@@ -341,6 +342,7 @@ for cluster in np.unique(clusters_train):
         n_iter=50,
         cv=5,
         n_jobs=-1,
+        n_points=5,
         scoring='f1',
         random_state=42
     )
@@ -387,6 +389,7 @@ bayes_cv = BayesSearchCV(
     n_iter=10,
     cv=3,
     n_jobs=-1,
+    n_points=5,
     scoring='f1_weighted',
     random_state=42
 )
