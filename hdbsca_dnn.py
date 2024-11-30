f8fcd44e484b97e8be77c76f2eaea7475729358f
abdussaboor98 <gaffari98@gmail.com>
2024-11-30 15:52:43 +0400

    cuda log

--- hdbsca_dnn.py
+++ hdbsca_dnn.py
@@ -17,6 +17,7 @@ from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
 
 # Check if CUDA is available
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
 
 # Define DNN model
 class FlightDelayDNN(nn.Module):
